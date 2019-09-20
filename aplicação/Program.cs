620ac5b16d1d177c7f30d677c802cfd960d13f71
fabiane santos <fabianesantossantos954@gmail.com>
2019-09-20 17:32:18 -0300

    exercicios 20/09

---
+++ aplicação/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace aplicação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int num = 1;
+                        
+           
+          while (num != 0){
+              Console.Write("Digite um número ou zero para parar:");
+            num = int.Parse(Console.ReadLine());
+             
+             if ((num % 2) != 0){
+                 Console.WriteLine("O número é par.");
+             } else {
+                 Console.WriteLine("O número é impar");
+             }
+          }
+            
+        }
+    }
+}
---
+++ atividade4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace atividade4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  int num = 5;
+
+            for (int num = 5;num * 5; num+= 5){
+             Console.WriteLine( num * " ");
+            }
+        }
+    }
+}
