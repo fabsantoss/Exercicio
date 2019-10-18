912b3f9e808d4cc96d5125ce909cfc6e9f7fc469
fabiane santos <fabianesantossantos954@gmail.com>
2019-10-18 17:20:12 -0300

    Exercicio 18/10

---
+++ ByteBank2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ByteBank2.Models;
+
+namespace ByteBank2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaCorretnte1 = new ContaBancaria(1, 1, "Alexandre");
+            ContaBancaria contaCorretnte1 = new ContaBancaria(1, 2, "Cesar");
+            
+        }
+    }
+}
