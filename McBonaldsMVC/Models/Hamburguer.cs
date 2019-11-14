1ba0f4c241eee93c6db8aa7dee031f817a8b9f55
fabiane santos <fabianesantossantos954@gmail.com>
2019-11-14 17:23:38 -0300

    Exercicio 14

--- McBonaldsMVC/Models/Hamburguer.cs
+++ McBonaldsMVC/Models/Hamburguer.cs
@@ -1,7 +1,20 @@
+using Microsoft.Extensions.Primitives;
+
 namespace McBonaldsMVC.Models
 {
     public class Hamburguer : Produto
     {
-        
+        private StringValues stringValues;
+        private double v;
+
+        public Hamburguer()
+        {
+        }
+
+        public Hamburguer(StringValues stringValues, double v)
+        {
+            this.stringValues = stringValues;
+            this.v = v;
+        }
     }
 }
